5ddb731958a13aabbab8b96506e13c1709273c4c
Daniel Vinciguerra <daniel.vinciguerra@resultadosdigitais.com.br>
2020-02-05 13:39:17 -0300

    First commit :thumbsup:

---
+++ lib/gitignore.rb
@@ -0,0 +1,6 @@
+require "gitignore/version"
+
+module Gitignore
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/gitignore_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Gitignore do
+  it "has a version number" do
+    expect(Gitignore::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
